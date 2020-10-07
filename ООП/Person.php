0526a1702f3dc572521d5d45dbb8eee48562ea27
grisha136 <71751494+grisha136@users.noreply.github.com>
2020-10-07 09:24:02 +0300

    Create Person.php

---
+++ ООП/Person.php
@@ -0,0 +1,42 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+class Person{
+  private $name;
+  private $lasname;
+  private $age;
+  private $mother;
+  private $father;
+  function __construct($name,$lastname,$age,$mother=null,$father=null){
+    $this->name = $name;
+    $this->lastname = $lastname;
+    $this->age = $age;
+    $this->mother = $mother;
+    $this->father = $father;
+  }
+  public function getName(){return $this->name;}
+  public function getLastame(){return $this->lastname;}
+  public function getAge(){return $this->age;}
+  public function getMother(){return $this->mother;}
+  public function getFather(){return $this->father;}
+  public function getInfo(){
+     return "
+     Меня зовут: ".$this->name."<br>
+     Мою маму зовут: ".($this->getMother()->getName())."<br>
+     Дедушку моей мамы зовут: ".$this->getMother()->getFather()->getName()."<br>
+     Бабушку моей мамы зовут: ".($this->getMother()->getMother()->getName())."<br>
+     Моего папу зовут: ".($this->getFather()->getName())."<br>
+     Дедушку моего папы зовут: ".($this->getFather()->getFather()->getName())."<br>
+     Бабушку моего папы зовут: ".$this->getFather()->getMother()->getName();
+  }
+}
+$ksenya = new Person("Ксения","Петрова", 61);
+$nikolay = new Person("Николай","Петров", 61);
+$anna = new Person("Анна","Викторова", 62);
+$petr = new Person("Петр","Викторов", 62);
+$oleg = new Person("Олег","Петров", 41);
+$olga = new Person("Ольга","Петрова", 41);
+$oleg = new Person("Олег","Петров", 41, $ksenya, $nikolay);
+$olga = new Person("Ольга","Петрова", 41, $anna , $petr);
+$igor = new Person("Игорь","Петров", 11, $olga, $oleg);
+echo $igor->getInfo();
+?>
