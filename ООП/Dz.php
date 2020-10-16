d73ec15a8755361fd8198ed13da1337aca9c00dc
grisha136 <71751494+grisha136@users.noreply.github.com>
2020-10-16 11:21:10 +0300

    Create Dz.php

---
+++ ООП/Dz.php
@@ -0,0 +1,33 @@
+<?
+
+abstract class Animal{
+  private $nickname;
+  private $age;
+  private $breed;
+  function __construct($nick,$age,$breed){
+    $this->nickname = $nick;
+    $this->age = $age;
+    $this->breed = $breed;
+  }
+  function getNickname(){return $this->nickname;}
+  function getAge(){return $this->age;}
+  function getBreed(){return $this->breed;}
+}
+
+class Horse extends Animal {
+    function run(){
+    echo $this-> getNickname()." Igogo, I rode. ";
+   }
+}
+
+class Pegasus extends Horse {
+  function fly(){
+    echo $this-> getNickname()." Igogo, I flew. ";
+  }
+}
+
+$bars = new Horse("Bars",4,null);
+$bars-> run();
+$kesha = new Pegasus("Kesha",5,null);
+$kesha-> fly();
+?>
